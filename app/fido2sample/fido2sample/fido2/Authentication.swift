7bda5394463d6f1ce12001499c661d8ff902033c
jenny.tabile <jenny.tabile@cplcloud.com>
2026-08-14 18:38:56 +0800

    ios:app:revert implementation

--- app/fido2sample/fido2sample/fido2/Authentication.swift
+++ app/fido2sample/fido2sample/fido2/Authentication.swift
@@ -57,7 +57,6 @@ class Authentication: NSObject {
             // Create Authentication request providing the required credentials.
             /* 1 */
             ## Create Fido2 request with json String ##
-            let fidoRequest = try TGFFido2RequestFactory.request(jsonString)
             
             // Setup an instance of TGFFido2RespondArgsBuilder with registration request
             // Initialize all necessary UI delegates required by FIDO2 SDK.
@@ -66,29 +65,12 @@ class Authentication: NSObject {
             // As a means of convenience, the FIDO2 UI SDK provides a ClientConformer class which conforms to all necessary delegates of FIDO2 SDK
             /* 2 */
             ## Setup TGFFido2RespondArgsBuilder with UI delegates ##
-            let respondArgsBuilder = TGFFido2RespondArgsBuilder(request: fidoRequest, uiDelegate: clientConformer)
-            respondArgsBuilder.uiBiometricAuthenticatorDelegate = clientConformer
-            respondArgsBuilder.uiPasscodeAuthenticatorDelegate = clientConformer
-            respondArgsBuilder.passcodeAuthenticator = TGFPasscodeAuthenticator(delegate: clientConformer)
-            let respondArgs = respondArgsBuilder.respondArgs()
             
             // Retrieve the FIDO2 Registration response.
             // Handle on error or response
             /* 3 */
             ## Retrieve FIDO2 response ##
-            fido2Client?.respond(with: respondArgs) {(response, error) in
-                DispatchQueue.main.async {
-                    if let error = error {
-                        Logger.log(string: "Authentication Error:\n" + error.localizedDescription)
-                        completion(error)
-                        return
-                    } else {
-                        let responseString: String = response!.raw()
-                        Logger.log(string: "Authentication Response:\n" + responseString)
-                        completion(nil)
-                    }
-                }
-            }
+            
         } catch let error {
             DispatchQueue.main.async {
                 completion(error)
